211d2a8fa1ce72dff399385c832ffb2624535dcf
tonyzhao <tonyzhao1650@gmail.com>
2017-11-11 22:59:05 +0800

    learning...

--- client/App.js
+++ client/App.js
@@ -1,13 +1,67 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
-export default class App extends React.Component {
+// export default class App extends React.Component {
+//   constructor(props){
+//     super(props)
+//     this.state = {
+//       blinding: true
+//     }
+//     // setInterval(() => {
+//     //   this.setState(previousState =>
+//     //     ({blinding: !this.state.blinding})
+//     //   )
+//     // }, 2000)
+//   }
+//   render() {
+//     return (
+//       this.state.blinding &&
+//       <View style={styles.container}>
+//         <Text>Open up App.js to start working on your app!</Text>
+//         <Text>Changes you make will automatically reload.</Text>
+//         <Text>Shake your phone to open the developer menu.</Text>
+//         <Image
+//           source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'}}
+//           style={{width: 320, height:180}}
+//         />
+//       </View>
+//     );
+//   }
+// }
+
+// export default class FlexDimensionsBasics extends React.Component {
+//   render() {
+//     return (
+//       // Try removing the `flex: 1` on the parent View.
+//       // The parent will not have dimensions, so the children can't expand.
+//       // What if you add `height: 300` instead of `flex: 1`?
+//       <View style={{flex: 1}}>
+//         <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+//         <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+//         <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+//       </View>
+//     );
+//   }
+// }
+
+export default class PizzaTranslator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{padding: 10}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text})}
+          onSubmitEditing={(text) => console.log(text)}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(', ')}
+        </Text>
       </View>
     );
   }
